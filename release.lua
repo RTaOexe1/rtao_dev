a4a971d917505c30443631f445613d4e3a030aa8
RTaOexe1 <warutchamchoy789@gmail.com>
2025-07-28 01:32:30 +0700

    Update and rename release.lua.txt to release.lua

--- release.lua
+++ release.lua
@@ -6592,6 +6592,52 @@ function Library:CreateWindow(Config)
 
 	Library.Window = Window
 	table.insert(Library.Windows, Window)
+        task.spawn(function()
+	local UserInputService = game:GetService("UserInputService")
+	local GuiService = game:GetService("GuiService")
+
+	local windowFrame = Window.Root
+
+	if not windowFrame then return end
+
+	local dragging, dragInput, dragStart, startPos
+
+	local function update(input)
+		local delta = input.Position - dragStart
+		windowFrame.Position = UDim2.new(
+			startPos.X.Scale,
+			startPos.X.Offset + delta.X,
+			startPos.Y.Scale,
+			startPos.Y.Offset + delta.Y
+	 )
+	end
+
+	windowFrame.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			dragging = true
+			dragStart = input.Position
+			startPos = windowFrame.Position
+
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+
+	windowFrame.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement then
+			dragInput = input
+		end
+	end)
+
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
+end)					
 	InterfaceManager:SetTheme(Config.Theme)
 	Library:SetTheme(Config.Theme)
 
