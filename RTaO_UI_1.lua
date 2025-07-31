0733c70440db663f9f035e027ce5b170d9c5a857
RTaOexe1 <warutchamchoy789@gmail.com>
2025-07-31 11:47:47 +0700

    Update RTaO_UI_1.lua

--- RTaO_UI_1.lua
+++ RTaO_UI_1.lua
@@ -184,7 +184,7 @@ function Update:StartLoad()
 	MainLoaderFrame.BorderSizePixel = 0;
 	local TitleLoader = Instance.new("TextLabel");
 	TitleLoader.Parent = MainLoaderFrame;
-	TitleLoader.Text = "RTaO";
+	TitleLoader.Text = "RTaO Dev";
 	TitleLoader.Font = Enum.Font.FredokaOne;
 	TitleLoader.TextSize = 50;
 	TitleLoader.TextColor3 = Color3.fromRGB(255, 255, 255);
@@ -321,7 +321,7 @@ function Update:Window(Config)
 	local keybind = keybind or Enum.KeyCode.RightControl;
 	local yoo = string.gsub(tostring(keybind), "Enum.KeyCode.", "");
 	local RTaO = Instance.new("ScreenGui");
-	RTaO.Name = "RTaO";
+	RTaO.Name = "RTaO Dev";
 	RTaO.Parent = game.CoreGui;
 	RTaO.DisplayOrder = 999;
 	local OutlineMain = Instance.new("Frame");
